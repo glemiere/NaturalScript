7eddcb007f033fc81650cd2c12561b686a179cf4
glemiere <guillaumelm.in@gmail.com>
2020-12-11 22:12:42 -0700

    Basic structure

---
+++ demo/logic.js
@@ -0,0 +1,9 @@
+const { Line } = require("../lib");
+
+const worst = 'WRST';
+
+Line("test", async function(arg1, arg2) {
+	console.log('Callback Called!');
+	console.log('Argument 1:', arg1);
+	console.log('Argument 2:', arg2);
+});

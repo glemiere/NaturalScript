ae27a163af4dd56615e7a5c94d9104097835a3f9
glemiere <guillaumelm.in@gmail.com>
2020-12-11 23:56:28 -0700

    Correct file paths

--- index.js
+++ index.js
@@ -7,9 +7,15 @@
 
 const path = require("path");
 const fs = require("fs");
+const readline = require('readline');
 
 const green = "\x1b[32m";
 const white = "\x1b[37m";
+
+const asyncFilter = async (arr, predicate) => {
+	const results = await Promise.all(arr.map(predicate));
+	return arr.filter((_v, index) => results[index]);
+}
 
 const getFilePaths = async function (startPath, filter, filepaths) {
     if (!fs.existsSync(startPath)) return;
@@ -28,10 +34,45 @@ const getFilePaths = async function (startPath, filter, filepaths) {
     return filepaths;
 };
 
-const main = async () => {
-    let jsFilePaths = await getFilePaths(path.join(__dirname, "./"), ".js", []);
+const checkFileForOccurence = async (filePath, string) => {
+	const fileStream = fs.createReadStream(filePath);
 
-    console.log(jsFilePaths);
+	const rl = readline.createInterface({
+		input: fileStream,
+		crlfDelay: Infinity
+	});
+
+	for await (const line of rl)
+		if (line.includes(string))
+	 		return true;
+	return false;
+};
+
+const filteringFilePathsUsingString = async (filePaths, string) => {
+	const filter = async (filePath) => {
+		const isFileContainingString = await checkFileForOccurence(filePath, string);
+		return isFileContainingString;
+	};
+
+	const filtered = await asyncFilter(filePaths, filter);
+
+	return filtered;
+};
+
+const getStratFiles = async () => {
+    let jsFilePaths = await getFilePaths(path.join(__dirname, "./"), ".js", []);
+	
+	// Excluding this file.
+	jsFilePaths = jsFilePaths.filter((fpath) => !fpath.includes(path.join(__dirname, "index.js")));
+	
+	let stratFiles = await filteringFilePathsUsingString(jsFilePaths, "const { Line } = ");
+    return stratFiles;
+};
+
+const main = async () => {
+	const stratFiles = await getStratFiles();
+
+	console.log(stratFiles);
 };
 
 main();
